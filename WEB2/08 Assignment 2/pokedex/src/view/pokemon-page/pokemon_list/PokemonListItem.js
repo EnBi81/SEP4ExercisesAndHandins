190e9e08b138dea77f93036e5bd0be0fe6be798d
Enbi81 <wotng2002@gmail.com>
2023-04-16 14:20:09 +0200

    Web2 Assignment 2: adding even cooler list item hover animation

--- WEB2/08 Assignment 2/pokedex/src/view/pokemon-page/pokemon_list/PokemonListItem.js
+++ WEB2/08 Assignment 2/pokedex/src/view/pokemon-page/pokemon_list/PokemonListItem.js
@@ -55,16 +55,18 @@ export default function PokemonListItem({pokemon, selected = false, setSelectedP
     let hiddenCss = state.calculatedColors ? '' : ' pokemon-list-item-hidden';
 
     return (
-        <div className={'pokemon-list-item ' + selectedClass + hiddenCss}
-             style={pokemonStyle}
-             onClick={onClick}
-             data-pokemon-id={pokemon.id}>
-            <div className='pokemon-simple-img'>
-                <img src={pokemon.image} onLoad={onImageLoad} onError={onImageError} ref={imageRef} alt=""/>
-            </div>
-            <div className='pokemon-simple-data'>
-                <div className='pokemon-simple-name'>{pokemon.name}</div>
-                <div className='pokemon-simple-id'><span>#</span><span>{pokemon.id}</span></div>
+        <div className={'pokemon-list-item-outer'}>
+            <div className={'pokemon-list-item ' + selectedClass + hiddenCss}
+                 style={pokemonStyle}
+                 onClick={onClick}
+                 data-pokemon-id={pokemon.id}>
+                <div className='pokemon-simple-img'>
+                    <img src={pokemon.image} onLoad={onImageLoad} onError={onImageError} ref={imageRef} alt=""/>
+                </div>
+                <div className='pokemon-simple-data'>
+                    <div className='pokemon-simple-name'>{pokemon.name}</div>
+                    <div className='pokemon-simple-id'><span>#</span><span>{pokemon.id}</span></div>
+                </div>
             </div>
         </div>
     )
