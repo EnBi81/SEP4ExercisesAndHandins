b89c503ca0ae645c8a95acfb1a8c4813a08d0e94
Enbi81 <wotng2002@gmail.com>
2023-04-17 18:43:58 +0200

    Web2 Assignment 2: writing a better about us page just because i can

--- WEB2/08 Assignment 2/pokedex/src/view/about-us/about-us.js
+++ WEB2/08 Assignment 2/pokedex/src/view/about-us/about-us.js
@@ -9,10 +9,18 @@ export function AboutUs(){
             <div className="about-us-text-content">
                 <h1>About us</h1>
                 <div>
+                    This website was created for a <b>school assignment</b>, but that ain't stop me making it fancy.
+                    It was designed only for desktop users, on mobile the gui is glitched all the way through.
+                    If you have any suggestions <i>(pls don't suggest to make it mobile friendly)</i>, feel free to check
+                    out the github repo and open an issue :D
+                </div>
+                <br/>
+                <div>
                     This website uses <a href={'https://pokeapi.co/'}>PokeApi</a> for retrieving data of the pokemon.
                 </div>
+                <br/>
                 <div>
-                    Feel free to check out the project files <a href={'https://github.com/EnBi81/SEP4ExercisesAndHandins/tree/main/WEB2/08%20Assignment%202/pokedex'}>here</a>, or
+                    Feel free to check out the <a href={'https://github.com/EnBi81/SEP4ExercisesAndHandins/tree/main/WEB2/08%20Assignment%202/pokedex'}>project files</a>, or
                     the <a href={'https://github.com/EnBi81/SEP4ExercisesAndHandins/tree/main/WEB2/08%20Assignment%202'}>assignment description</a>.
                 </div>
             </div>
